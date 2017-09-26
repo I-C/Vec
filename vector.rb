61dc8a0b039dbdd4a626210f50cda0df11039cb0
j1303 <j1303@PC4-43.cc.matsue-ct.jp>
2017-09-26 11:58:10 +0900

    vector.rb kadai4mode

--- vector.rb
+++ vector.rb
@@ -1,13 +1,16 @@
 class Vector
+  attr_accessor :x, :y
   def initialize(x,y)
     @x = x
     @y = y
   end
   def to_s
-    #p "(#{@x},#{@y})"
     return "(#{@x},#{@y})"
   end
   def length
     return Math::sqrt(@x**2+@y**2)
+  end
+  def add(name)
+    Vector.new(name.x + @x,name.y + @y)
   end
 end
